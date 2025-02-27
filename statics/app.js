fd54236833febf35f1bbf1ba5ed4cc31e1c3c5eb
Eter Nada <123267672+tarenjk24@users.noreply.github.com>
2025-02-27 16:38:17 +0000

    update

--- statics/app.js
+++ statics/app.js
@@ -1,3 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Use buttons to toggle between views
+    document.querySelector('#menu').addEventListener('click', () => load_menu());
+    document.querySelector('#aboutus').addEventListener('click', () => load_aboutus());
+
+    // By default, load the menu
+    load_menu();
+});
+
+// Function to load menu
+function load_menu() {
+    // Show the menu and hide other views
+    document.querySelector('#menu').style.display = 'block';
+    document.querySelector('#aboutus').style.display = 'none';
+}
+
+// Function to load aboutus
+function load_aboutus() {
+    // Show the aboutus and hide other views
+    document.querySelector('#menu').style.display = 'none';
+    document.querySelector('#aboutus').style.display = 'block';
+}
+
+
 // Function to save missed days
 function saveMissedDays() {
     const missedDays = document.getElementById('missedDays').value;
